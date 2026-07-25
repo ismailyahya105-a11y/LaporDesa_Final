5aa573d5e11d1f078a79f0e88f410e4e5aa473bb
yahya <ismailyahya105@gmail.com>
2026-07-25 18:44:08 +0700

    Add smart village API routes

--- routes/api.php
+++ routes/api.php
@@ -4,12 +4,23 @@ use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\KategoriController;
 use App\Http\Controllers\Api\LaporanController;
 use App\Http\Controllers\Api\TanggapanController;
+use App\Http\Controllers\Api\SmartVillageController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/forgot-password', [AuthController::class, 'forgotPassword']);
+Route::post('/reset-password', [AuthController::class, 'resetPassword']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/informasi', [SmartVillageController::class, 'informasi']);
+    Route::get('/surat', [SmartVillageController::class, 'surat']); Route::post('/surat', [SmartVillageController::class, 'storeSurat']);
+    Route::get('/darurat', [SmartVillageController::class, 'darurat']); Route::post('/darurat', [SmartVillageController::class, 'storeDarurat']);
+    Route::get('/pasar', [SmartVillageController::class, 'pasar']);
+    Route::get('/usulan', [SmartVillageController::class, 'usulan']); Route::post('/usulan', [SmartVillageController::class, 'storeUsulan']);
+    Route::get('/polling', [SmartVillageController::class, 'polling']); Route::post('/polling/{polling}/vote', [SmartVillageController::class, 'votePolling']);
+    Route::get('/peta-desa', [SmartVillageController::class, 'peta']);
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/profile', [AuthController::class, 'profile']);
 
